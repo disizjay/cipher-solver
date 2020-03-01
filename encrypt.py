277e2d4945cb800d64a07098c3c6b418fa71d428
Manikumar Juttukonda <manikumar.juttukonda@gmail.com>
2020-03-01 14:10:58 -0500

    second commit

---
+++ encrypt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import random
+import string
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+key = list(abc)
+random.shuffle(key)
+key = "".join(key)
+
+text = open('plaintext_read.txt').read().lower()
+
+trans = string.maketrans(abc, key)
+
+f = open("plainencrypted.txt", "a")
+f.write(text.translate(trans))
+f.close()
+
+#print text.translate(trans)
